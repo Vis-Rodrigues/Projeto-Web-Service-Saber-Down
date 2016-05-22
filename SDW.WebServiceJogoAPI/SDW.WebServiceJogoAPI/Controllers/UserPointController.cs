d2f2a88cfca4ca68032cae10dda0f3ab354551e8
Vis-Rodrigues <vi_ale10@hotmail.com>
2016-05-22 12:36:10 -0300

    Adicionado UserPoint

--- SDW.WebServiceJogoAPI/SDW.WebServiceJogoAPI/Controllers/UserPointController.cs
+++ SDW.WebServiceJogoAPI/SDW.WebServiceJogoAPI/Controllers/UserPointController.cs
@@ -28,16 +28,16 @@ namespace SDW.WebServiceJogoAPI.Controllers
         {
            foreach(Pontuacao p in _unit.PontuacaoRepository.Listar()){
                 UserPoint userPoint = new UserPoint();
-                userPoint.PontuacaoId = p.PontuacaoId;
+                userPoint.PontuacaoId = 1;
                 userPoint.Moeda = 0;
                 userPoint.Ponto = 0;
                 userPoint.UsuarioId = 4;
                 Usuario u = new Usuario();
                 u = _unit.UsuarioRepository.BuscarPorCodigo(4);
-                userPoint.Descricao = u.Descricao;
-                userPoint.Email = u.Email;
-                userPoint.Genero = u.Genero;
-                userPoint.Senha = u.Senha;
+                userPoint.Descricao = "teste";
+                userPoint.Email = "teste@gmail.com";
+                userPoint.Genero = "0";
+                userPoint.Senha = "123456";
                 lista.Add(userPoint);
             }
             return lista;
